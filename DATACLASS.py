a787085f4592bf5579d5b6ba46e27cd2eeb44c99
Abdrahamane Sarambounou <absara927@gmail.com>
2021-06-19 22:31:37 +0000

    Refactoring with pycharm

--- DATACLASS.py
+++ DATACLASS.py
@@ -1,18 +1,16 @@
-
-import os, json, logging, tempfile
-from _vars import loadJson, dumpJson
-
+import json
+import logging
+import os
+import tempfile
 
 logging.basicConfig(filename='journal.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s --> %(message)s')
 logging.debug('Exécution du programme reussie !')
 
 
 class DATA:
-
     tempdir = tempfile.mkdtemp()
     jsonFilePath = os.path.join(tempdir + '/Data.json')
 
-    
     # def __str__(self):
     #     _dict = {self.siteName: {'Login': self.login, 'Password': self.password}}
     #     return _dict
@@ -21,51 +19,51 @@ class DATA:
     def get_data(cls):
         if os.path.exists(cls.jsonFilePath):
             with open(cls.jsonFilePath, "r") as f:
-                dataDict = json.load(f)
+                data_dict = json.load(f)
+            return data_dict
+
     @classmethod
     def save_data(cls, datas):
         with open(cls.jsonFilePath, 'w') as f:
             json.dump(datas, f, indent=4)
 
     @staticmethod
-    def add_data(siteName, login, password):
+    def add_data(site_name, login, password):  # sourcery skip: dict-literal
         datas = DATA.get_data()
-        datas = dict()
-        if siteName not in datas:
-            datas[siteName] = {'Login': login, 'Password': password}
+        if site_name not in datas:
+            datas[site_name] = {'Login': login, 'Password': password}
             DATA.save_data(datas)
             logging.info("Enregistrement Effectuée")
             return True
         else:
-            logging.warning(f'le site {siteName} existe déja')
+            logging.warning(f'le site {site_name} existe déja')
             return False
 
     @staticmethod
-    def remove_data(nomSite):
+    def remove_data(nom_site):
         datas = DATA.get_data()
-        if nomSite in datas:
-            del datas[nomSite]
+        if nom_site in datas:
+            del datas[nom_site]
             DATA.save_data(datas)
             logging.info('Suppression Effectuée')
             return True
         else:
-            logging.warning(f'Erreur Supression, le site {nomSite} est introuvable')
+            logging.warning(f'Erreur Supression, le site {nom_site} est introuvable')
             return False
-            
+
     @staticmethod
-    def set_data(siteName, newLogin, newPass):
+    def set_data(site_name, new_login, new_pass):
+        # sourcery skip: dict-literal, remove-dict-keys
         datas = DATA.get_data()
-        datas = dict()
-        if siteName in datas.keys():
-            datas[siteName] = {"Login":newLogin, "Password":newPass}
+        if site_name in datas.keys():
+            datas[site_name] = {"Login": new_login, "Password": new_pass}
             logging.info("Modification Effectuée")
-            DATA.save_data(datas) 
+            DATA.save_data(datas)
             return True
         else:
-            logging.warning(f"le site {siteName} n'est pas dans la base")
+            logging.warning(f"le site {site_name} n'est pas dans la base")
             return False
 
 
-    
 if __name__ == '__main__':
     print(DATA.jsonFilePath)
